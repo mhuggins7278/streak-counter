d9ceac2968f1fa7a8821b641cf33476eab83e7bb
Mark Huggins <mhuggins@glgroup.com>
2022-07-24 23:19:48 -0400

    lesson 2 section 3

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,64 @@
-import {formattedDate} from "./utils"
+import {formattedDate, buildStreak, Streak, updateStreak} from "./utils"
+
 
 const KEY = "streak"
 
-interface Streak {
-  currentCount: number
-  startDate: string
-  lastLoginDate: string
-}
 
 export function streakCounter(storage: Storage, date: Date): Streak {
   const streakInLocalStorage = storage.getItem(KEY)
 
   if (streakInLocalStorage) {
     try {
-      const streak = JSON.parse(streakInLocalStorage || "")
-      return streak
+      const streak = JSON.parse(streakInLocalStorage) as Streak
+      const state = shouldIncrementOrResetStreakCount(
+        date,
+        streak.lastLoginDate
+      )
+      const SHOULD_INCREMENT = state === "increment"
+      const SHOULD_RESET = state === "reset"
+
+      if (SHOULD_INCREMENT) {
+        const updatedStreak = buildStreak(date, {
+          startDate: streak.startDate,
+          currentCount: streak.currentCount + 1,
+          lastLoginDate: formattedDate(date),
+        }) 
+        updateStreak(storage, updatedStreak)
+        return updatedStreak
+      }
+      if (SHOULD_RESET) {
+        const updatedStreak = buildStreak(date)
+        updateStreak(storage, updatedStreak)
+        return updatedStreak
+      }
+
     } catch (error) {
       console.error("Failed to parse streak from localStorage")
     }
   }
 
-  const streak = {
-    currentCount: 1,
-    startDate: formattedDate(date),
-    lastLoginDate: formattedDate(date),
-  }
+  const streak =  buildStreak(date)
 
-  storage.setItem(KEY, JSON.stringify(streak))
+  updateStreak(storage, streak)
   return streak
+}
+
+function shouldIncrementOrResetStreakCount(
+  currentDate: Date,
+  lastLoginDate: string
+): "increment" | "reset" | "none" {
+  // We get 11/5/2021
+  // so to get 5, we split on / and get the second item
+  const difference =
+    currentDate.getDate() - parseInt(lastLoginDate.split("/")[1])
+  if (difference === 0) {
+    return "none"
+  }
+  // This means they logged in the day after the currentDate
+  if (difference === 1) {
+    return "increment"
+  }
+  // Otherwise they logged in after a day, which would
+  // break the streak
+  return "reset"
 }
